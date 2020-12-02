a6865bed989da0a458e2abe7212a4f27018c19b2
rikto1 <74736264+rikto1@users.noreply.github.com>
2020-12-03 03:19:42 +0600

    Add files via upload

---
+++ question.js
@@ -0,0 +1,30 @@
+
+
+//Array of Objects
+const quiz = [
+{
+    q:'কোনটি প্রাকৃতিক পরিবেশের উপাদান  ?',
+    options:['গাছ','টেবিল','কলম','চেয়ার'],
+    answer:0
+},
+{
+    q: 'কোনটি মানুষের তৈরি পরিবেশের উপাদান ?',
+    options:['পাখি','পাহাড়','ঘরবাড়ি','মাছ'],
+    answer:2
+},
+{
+    q: '3 + 4 = 7 ?',
+    options:['true','false'],
+    answer:0
+},
+{
+    q: 'Which time of day do we have breakfast ?',
+    options:['In the afternoon','In the evening','In the morning'],
+    answer:2
+},
+{
+    q: 'What is 22 + 6 ?',
+    options:['99','56','16','28'],
+    answer:3
+}
+]
